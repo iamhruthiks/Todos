5013b1f5e29975a500a37fb8efb60c2d928de2eb
iamhruthiks <iamhruthiks@gmail.com>
2025-03-21 08:09:20 +0530

    [backend] setup mongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
@@ -11,6 +12,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
+
+// connect to mongoDB database
+connectDB();
 
 // routes
 app.get("/", (req, res) => {
