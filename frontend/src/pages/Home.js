540497653fdc60a468e12f04943bd7cdfd30c683
iamhruthiks <iamhruthiks@gmail.com>
2025-03-23 23:52:48 +0530

    [frontend] add animation in home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,9 +2,29 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome To Todos!</h1>
-    </div>
+    <>
+      <div className="sp-container">
+        <div className="sp-content">
+          <div className="sp-globe"></div>
+          <h2 className="frame-1">AWESOME</h2>
+          <h2 className="frame-2">WELCOME TO TODOS!</h2>
+          <h2 className="frame-3">BUILD WITH MERN STACK</h2>
+          <h2 className="frame-4">TEST IT!</h2>
+          <h2 className="frame-5">
+            <span>PLAN,</span>
+            <span> TRACK,</span>
+            <span> ACHIEVE.</span>
+          </h2>
+          <a
+            className="sp-circle-link"
+            target="blank"
+            href="https://www.linkedin.com/in/hruthiks/"
+          >
+            by Hruthik S
+          </a>
+        </div>
+      </div>
+    </>
   );
 };
 
