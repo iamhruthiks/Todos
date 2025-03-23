da61d8754ae966dcbc154ced3e489d387da20987
iamhruthiks <iamhruthiks@gmail.com>
2025-03-23 13:09:51 +0530

    [frontend] implement sorting by priority feature

--- frontend/src/pages/AllTodos.js
+++ frontend/src/pages/AllTodos.js
@@ -3,6 +3,8 @@ import { fetchAllTodos } from "../services/Api";
 
 const AllTodos = () => {
   const [todos, setTodos] = useState([]);
+  const [selectedPriority, setSelectedPriority] = useState(null);
+
   const gradients = [
     "linear-gradient(to right, #2193b0, #6dd5ed)",
     "linear-gradient(to right, #e9b7ce, #d3f3f1)",
@@ -13,17 +15,62 @@ const AllTodos = () => {
   const getRandomGradient = () =>
     gradients[Math.floor(Math.random() * gradients.length)];
 
-  useEffect(() => {
-    const getTodos = async () => {
-      const fetchedTodos = await fetchAllTodos();
-      setTodos(fetchedTodos);
-    };
+  // fetch todos from the backend
+  const getTodos = async (priority = null) => {
+    const filters = priority ? { priority } : {};
+    const fetchedTodos = await fetchAllTodos(filters);
+    setTodos(fetchedTodos);
+  };
 
-    getTodos();
+  useEffect(() => {
+    getTodos(); // initailly fecth all todos
   }, []);
+
+  // fetch tods based on todo priority
+  const handlePriorityClick = (priority) => {
+    setSelectedPriority(priority);
+    getTodos(priority);
+  };
 
   return (
     <div className="container mt-4">
+      {/* priority sorting buttons */}
+      <div className="row d-flex justify-content-center align-items-center mb-4">
+        <div
+          className={`col priority-label bg-primary text-white ms-3 ${
+            selectedPriority === null ? "selected" : ""
+          }`}
+          onClick={() => handlePriorityClick(null)}
+        >
+          All Todos
+        </div>
+        <div
+          className={`col priority-label low ${
+            selectedPriority === "low" ? "selected" : ""
+          }`}
+          onClick={() => handlePriorityClick("low")}
+        >
+          Low
+        </div>
+        <div
+          className={`col priority-label medium ${
+            selectedPriority === "medium" ? "selected" : ""
+          }`}
+          onClick={() => handlePriorityClick("medium")}
+        >
+          Medium
+        </div>
+        <div
+          className={`col priority-label high me-3 ${
+            selectedPriority === "high" ? "selected" : ""
+          }`}
+          onClick={() => handlePriorityClick("high")}
+        >
+          High
+        </div>
+      </div>
+
+      {/* display todos */}
       <div className="row">
         {todos.map((todo) => (
           <div key={todo._id} className="col-md-4">
@@ -38,7 +85,7 @@ const AllTodos = () => {
                 </h6>
                 <p className="card-text">
                   <strong>Created By:</strong>{" "}
-                  {todo.userId.username || "Not Found"}
+                  {todo.userId?.username || "Not Found"}
                 </p>
                 <p className="card-text">
                   <strong>Tags:</strong> {todo.tags.join(", ") || "No Tags"}
--- frontend/src/services/Api.js
+++ frontend/src/services/Api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 const API_BASE_URL = process.env.REACT_APP_API_URL;
 
-export const fetchAllTodos = async () => {
+export const fetchAllTodos = async (filters = {}) => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/todos`);
+    const queryParams = new URLSearchParams(filters).toString();
+    const response = await axios.get(`${API_BASE_URL}/todos?${queryParams}`);
     return response.data.todos;
   } catch (error) {
     console.error("Error fetching todos:", error);
